2613ed7f75f1e048536b0915efe2325605e624b5
Liyo58 <Liyomdbot58@gmail.com>
2024-10-18 17:49:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=P6JwgYzS#545eBu9Xtg12QTZJDH9XN8WCeNKQpuQHGk_DlH6NkGk",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=S7oEjTAD#9wjVzwSaGP2bRpEIPhEh7EXPHEnhTIzyrojVcnrunkM",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
